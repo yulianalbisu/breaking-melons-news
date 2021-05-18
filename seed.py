2277fa187fef8c8ea5c596ef7068fbc25ca0e3e5
ekmerline <emilykmerline@gmail.com>
2021-05-18 09:05:07 -0500

    Updated seed.py and adjusted issue with model.py.

--- seed.py
+++ seed.py
@@ -6,13 +6,15 @@ import model
 import server
 import json
 
-os.system('dropdb testmelonnews')
-os.system('createdb testmelonnews')
 
-model.connect_to_db(server.app)
-model.db.create_all()
 
 def example_data():
+
+    os.system('dropdb testmelonnews')
+    os.system('createdb testmelonnews')
+
+    model.connect_to_db(server.app)
+    model.db.create_all()
 
     f = open('seeddata.json',)
 
@@ -25,9 +27,9 @@ def example_data():
     model.User.query.delete()
     model.Category.query.delete()
 
-    users_in_db = []
-    categories_in_db = []
-    news_in_db = []
+    users_in_db = {}
+    categories_in_db = {}
+    news_in_db = {}
 
     for user in data['users']:
         db_user = crud.create_user(
@@ -36,19 +38,18 @@ def example_data():
             user_role=user['user_role'],
             password=user['password']
         )
-        users_in_db.append(db_user)
+        users_in_db[db_user.email] = db_user
 
     for category in data['categories']:
         db_category = crud.create_category(
             category_type=category['category_type'], 
             description=category['description'])
-        categories_in_db.append(db_category)
+        categories_in_db[db_category.category_type] = db_category
 
-    i=0
     for news in data['news']:
         db_news = crud.create_news(
-            user=users_in_db[0],
-            category=categories_in_db[i], 
+            user=users_in_db[news['email']],
+            category=categories_in_db[news['category_type']], 
             title=news['title'], 
             summary=news['summary'], 
             article_text=news['article_text'], 
@@ -56,9 +57,13 @@ def example_data():
             picture_link=news['picture_link'], 
             date_post=datetime(news['date_post'][0], news['date_post'][1], news['date_post'][2])
             )
-        i+=1
-        news_in_db.append(db_news)
+        news_in_db[db_news.title] = db_news
 
-    crud.create_comment(user=users_in_db[1], news=news_in_db[0], comment_text=data['comments'][0]['comment_text'])
+    for comment in data['comments']:
+        crud.create_comment(
+            user=users_in_db[comment['email']], 
+            news=news_in_db[comment['title']], 
+            comment_text=comment['comment_text']
+            )
 
 example_data()
